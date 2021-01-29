2d825afb4a177ee76eff96af1d13611d259f6623
Jean-Christophe MARQUET <Marnalas@users.noreply.github.com>
2021-01-29 16:44:51 +0100

    Saisie en dur de l'URL de l'API en attendant de pouvoir changer la variable d'environnement.

--- front/src/utils/rest-module.js
+++ front/src/utils/rest-module.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL: 'https://traitdunion.api.recette.grandest.fr/api/v1'
+  //baseURL: process.env.REACT_APP_API_BASE_URL
 })
 
 export default axiosInstance
