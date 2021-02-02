8307b70086a24a4fd7f408626d9ca8f8ba47e15a
Sullivan BEAUGER <sullivan.beauger@sully-group.fr>
2021-02-02 17:04:00 +0100

    Remise en place de la variable d'environnement

--- front/src/utils/rest-module.js
+++ front/src/utils/rest-module.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: 'https://traitdunion.api.recette.grandest.fr/api/v1'
-  //baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL: process.env.REACT_APP_API_BASE_URL
 })
 
 export default axiosInstance
